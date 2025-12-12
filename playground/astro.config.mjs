1c9dfa7a9412d1f0d18f611cdc1a502bfd88cfe4
Clément Roche <rchclement@gmail.com>
2025-12-12 11:06:49 +0100

    Update sponsor list in README.md

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import vue from '@astrojs/vue'
 
+
 export default defineConfig({
   integrations: [react(), vue({ appEntrypoint: './vue/setup' })],
   devToolbar: {
     enabled: false,
   },
   srcDir: './www',
-})
+});
